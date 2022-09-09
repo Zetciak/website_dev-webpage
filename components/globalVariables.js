87477d93400f62f1de3de3c80e07822b4ddacdb6
Zetciak <97193808+Zetciak@users.noreply.github.com>
2022-09-09 20:01:58 +0200

    images

--- components/globalVariables.js
+++ components/globalVariables.js
@@ -5,7 +5,7 @@ const getVariable = {
 			'website-dev.pl - Tworzenie nowoczesnych stron internetowych',
 		pageDesc:
 			'Front-end developer & Junior UI/UX Designer, Tworzenie nowoczesnych stron internetowych w atrakcyjnych cenach. Zapoznaj się z naszą ofertą!',
-		pageImage: 'https://i.imgur.com/cCaTkp3.png',
+		pageImage: 'https://i.imgur.com/iowTddc.png',
 		pageLink: 'website-dev.pl',
 	},
 
@@ -13,7 +13,7 @@ const getVariable = {
 		pageTitle: 'website-dev.pl - Creating modern websites',
 		pageDesc:
 			'Front-end developer & Junior UI / UX Designer, Creating modern websites at attractive prices. Check out our offer!',
-		pageImage: 'https://i.imgur.com/dWlrcmA.png',
+		pageImage: 'https://i.imgur.com/YRC4qiE.png',
 		pageLink: 'website-dev.pl',
 	},
 
